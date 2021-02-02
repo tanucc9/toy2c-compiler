739b48168fb104aee2a638bdf6a40e4971430efd
tanucc <maurogaetano997@gmail.com>
2021-02-02 21:33:00 +0100

    Modifiche Tester

--- src/Tester.java
+++ src/Tester.java
@@ -13,17 +13,24 @@ import javax.xml.transform.stream.StreamResult;
 
 public class Tester {
     public static void main(String[] args) throws Exception {
-
-        File file=new File(args[0]);
-        InputStream in = new FileInputStream(file);
-        Reader reader = new InputStreamReader(in);
-
+       Reader reader = null;     
+        
+        try {
+                File file = new File(args[0]);
+                InputStream in = new FileInputStream(file);
+                reader = new InputStreamReader(in);
+            } catch (FileNotFoundException e) {
+                throw new Error ("Il file toy che stai cercando di compilare non è stato trovato.\n" +
+                        "Assicurati di aver inserito correttamente il path.");
+            }
+        
+        
         //Parser & lexer
         parser p = new parser(new Yylex(reader));
         //p.debug_parse();
         ProgramOP pOP= (ProgramOP) p.parse().value;
 
-        /*
+
         //Generazione ast in xml
         GenerateXML xml = new GenerateXML();
         Document xml_generated = (Document)pOP.accept(xml);
@@ -34,7 +41,7 @@ public class Tester {
         DOMSource domSource = new DOMSource(xml_generated);
         StreamResult streamResult = new StreamResult(new File(System.getProperty("user.dir")+"\\albero_sintattico.xml"));
         transformer.transform(domSource, streamResult);
-        */
+
 
         //Analisi semantica e ast esteso
         SemanticAnalisys sa = new SemanticAnalisys();
@@ -43,34 +50,24 @@ public class Tester {
         //Generazione codice c
         CGenerator cg= new CGenerator();
         String fileC = (String) cg.visit(astExt);
-        System.out.println(fileC);
-//        String codIntermedio=null;
-//        for(int i=0; i < args.length; i++){
-//            if(args[i].equals("-intc")) codIntermedio=args[i+1];
-//        }
-//
-//        String fileOutDir = "";
-//        if(codIntermedio != null){
-//            fileOutDir = System.getProperty("user.dir") + "/" + codIntermedio;
-//        }else{
-//            fileOutDir = System.getProperty("user.dir") + "/codIntermedio.c" ;
-//        }
 
-    /*    String fileOutDir = System.getProperty("user.dir") + "/" + args[1];
-        File f = new File(fileOutDir);
-        if(!f.exists()) {
-            if (f.createNewFile()) {
+        String fileOutDir = System.getProperty("user.dir") + "/" + args[1];
+
+            File f = new File(fileOutDir);
+            if(!f.exists()) {
+                if (f.createNewFile()) {
+                    FileWriter fw = new FileWriter(fileOutDir);
+                    fw.write(fileC);
+                    fw.close();
+                } else {
+                    throw new Error("Errore sulla generazione del file intermedio.");
+                }
+
+            } else {
                 FileWriter fw = new FileWriter(fileOutDir);
                 fw.write(fileC);
                 fw.close();
-            } else {
-                throw new Error("Errore sulla generazione del file intermedio.");
             }
 
-        } else {
-            FileWriter fw = new FileWriter(fileOutDir);
-            fw.write(fileC);
-            fw.close();
-        }*/
    }
 }
--- toy2c.bat
+++ toy2c.bat
@@ -1,10 +1,33 @@
 @Echo Off
-java -jar mauro-avolicino_es5_scg.jar %1 %2 %3
+IF "%~1" == "--help" (
+    echo *** HELP - TOY2C ***
+    echo *  Argomento 1, obbligatorio: nome file toy da compilare. ES. programmi_toy/file.toy
+    echo *  Argomento 2, opzionale: nome file c di output generato dal toy. ES. prog.c
+    echo *  Argomento 3, opzionale: nome file exe generato dal c. ES. out
+    GOTO :EOF
+)
+IF "%~1" == "" GOTO :noToyFile
+IF "%~2" == "" (
+SET C=prog.c
+GOTO :checkOut
+)
+SET C=%2%
+
+:checkOut
+IF "%~3" == "" (
+SET O=out
+GOTO :jarToy
+)
+SET O=%3%
+
+:jarToy
+java -jar out/artifacts/mauro_avolicino_es5_scg_jar2/mauro-avolicino_es5_scg.jar %1 %C%
 IF %errorlevel% NEQ 0 GOTO :EOF
-IF "%~3" == "" GOTO emptyExe
-gcc -pthread -lm -o %3 %2
-%3.exe
-:emptyExe
-gcc -pthread -lm %2
-a.exe
+echo Compilazione in corso...
+gcc -pthread -lm -o %O% %C%
+%O%.exe
+GOTO :EOF
+
+:noToyFile
+echo E' necessario passare almeno il nome del file.toy che si vuole compilare.
 
